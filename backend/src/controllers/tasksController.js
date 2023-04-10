e6ad730f6628d13cc9cd74489a85494739edd827
juliocjuri <juliocesarjuriolli@gmail.com>
2023-04-10 16:54:03 -0300

    createTask route

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -4,5 +4,12 @@ const getAll = async (req, res) => {
      const tasks = await tasksModel.getAll();
      return res.status(200).json(tasks)
 };
+
+const createTask = async (req, res) => {
+     // const createdTask = await tasksModel.createTask()
+     return res.status(201).json({message: 'ok'})
+}
 //44:27
-module.exports = {getAll};
+module.exports = {
+     getAll,
+     createTask};
--- backend/src/models/taskModel.js
+++ backend/src/models/taskModel.js
@@ -1,10 +1,24 @@
 const connection = require('./connection')
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM tasks');
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
+const createTask = async (task) => {
+    const { title } = task;
+
+    const dateUTC = new Date(Date.now()).toUTCString()
+
+    const query = 'INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)'
+
+    const createdTask = await connection.execute(query, [title, 'pendente', dateUTC])
+
+    console.log('createdTask')
+    return createdTask
+}
+
 module.exports = { 
-    getAll
+    getAll,
+    createTask
 };
